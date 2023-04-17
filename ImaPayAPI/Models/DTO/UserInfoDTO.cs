91d8d0d954256d84bb8903855fbc082394bf0d98
thiagonebuloni <thiago.nebuloni@gmail.com>
2023-04-17 19:16:44 -0300

    add user-info-dto

--- ImaPayAPI/Models/DTO/UserInfoDTO.cs
+++ ImaPayAPI/Models/DTO/UserInfoDTO.cs
@@ -2,6 +2,7 @@
 {
     public class UserInfoDTO
     {
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Account { get; set; } = string.Empty;
         public int Agency { get; set; }
